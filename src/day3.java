b47655b071125b6b45eb9f4ea8dcdda411f71dbb
JakeLW1 <65059140+JakeLW1@users.noreply.github.com>
2020-12-03 13:59:18 +0000

    day 3 solved

--- src/day3.java
+++ src/day3.java
@@ -5,39 +5,40 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class tabogganTrajectory {
+
+public class day3 {
 
 	public static void main(String[] args) {
 		
 		String fileName = "input.txt"; //find file
 		File file = new File(fileName); //make file into file
 		
-		List<String> map = new ArrayList<>();
-		List<String> bigMap = new ArrayList<>();
+		List<String> map = new ArrayList<>(); //small map
+		List<String> bigMap = new ArrayList<>(); //big map
 		Scanner inputStream; //start scanner
 		
 		try {
 			inputStream = new Scanner(file); //read file
 			
 			while(inputStream.hasNext()) {
-				String row = inputStream.next(); //assign number to string
-				String[] rows = row.split("[\r?\n|\r]+", -1); //assign strings to array split by new line
+				String row = inputStream.next(); //assign rows to string
+				String[] rows = row.split("[\r?\n|\r]+", -1); //assign strings to array split by new line (ooooooooo regex)
 				
-				map.addAll(Arrays.asList(rows));
+				map.addAll(Arrays.asList(rows)); //add rows to map
 			}
 			inputStream.close(); //close read
 		}catch(FileNotFoundException e) {
 			e.printStackTrace(); //error stuff
 		}
 		
-		for(int i = 0; i < map.size(); i++) {
-			String row = map.get(i);
-			row = map.get(i).repeat(57);
-			bigMap.add(row);
-			System.out.println(row);
+		for(int i = 0; i < map.size(); i++) { //loop through the whole map
+			String row = map.get(i); //get each row and make it a string
+			row = map.get(i).repeat(73); //repeat the string 73(!) times
+			bigMap.add(row); //add it to BIG map
+			System.out.println(row); //puke it out to console
 		}
 		
-		System.out.println(bigMap.get(0).length());
+		System.out.println(bigMap.get(0).length()); //debug by logging lol
 		
 		int treeMiss1 = 0;
 		int treeMiss2 = 0;
@@ -46,7 +47,7 @@ public class tabogganTrajectory {
 		int treeMiss5 = 0;
 		int treeHit1 = 0;
 		int treeHit2 = 0;
-		int treeHit3 = 0;
+		int treeHit3 = 0;           //i'm sorry god don't look at this
 		int treeHit4 = 0;
 		int treeHit5 = 0;
 		int checkpoint1 = 0;
@@ -55,6 +56,7 @@ public class tabogganTrajectory {
 		int checkpoint4 = 0;
 		int checkpoint5 = 0;
 		
+		//repeating for-loops, stepping through each row of map and jumping by however long the challenge wants
 		for(int i = 0; i < bigMap.size(); i++) {
 			for(int j = checkpoint2; j < bigMap.get(i).length(); j++) {
 				if(bigMap.get(i).charAt(j) == '.') {
@@ -120,10 +122,15 @@ public class tabogganTrajectory {
 			}
 			checkpoint5++;
 		}
-		long totalHits = treeHit1 * treeHit2 * treeHit3 * treeHit4 * treeHit5;
-		System.out.println("misses: " + treeMiss1*treeMiss2*treeMiss3*treeMiss4*treeMiss5 + ", hits: " + totalHits);
+		long totalHits = 1;
+		totalHits *= treeHit1;
+		totalHits *= treeHit2;
+		totalHits *= treeHit3;       //*multiplication*
+		totalHits *= treeHit4;
+		totalHits *= treeHit5;
+		System.out.println("misses: " + treeMiss1*treeMiss2*treeMiss3*treeMiss4*treeMiss5 + ", hits: " + totalHits); //answer goes here
 		System.out.println(treeHit1 + " " + treeHit2 + " " + treeHit3 + " " + treeHit4 + " " + treeHit5);
-		System.out.println(checkpoint5);
+		System.out.println(checkpoint5); //debug by printing you know the score
 	}
 
 }

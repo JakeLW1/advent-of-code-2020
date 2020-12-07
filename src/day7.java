20816a2235b0112dde7b7e7dd009d87eb426dabf
JakeLW1 <65059140+JakeLW1@users.noreply.github.com>
2020-12-07 22:21:59 +0000

    shoutout to joe pog day 7 done (not commented not pog)

--- src/day7.java
+++ src/day7.java
@@ -2,17 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class day7 {
 
 	public static void main(String[] args) {
 		
-		String fileName = "./resources/bags.txt"; //find file
+		String fileName = "bags.txt"; //find file
 		File file = new File(fileName); //make file into file
 		
 		List<String> bags = new ArrayList<>(); //empty string list
@@ -34,52 +31,35 @@ public class day7 {
 			e.printStackTrace(); //error stuff
 		}
 		
-		List<String> holder = new ArrayList<>();
-		holder.add("1 shiny gold");
 		int count = 0;
-		HashMap<String, Integer> quantities = new HashMap<>();
-		quantities.put("1 shiny gold", 1);
+		List<String> bagsToProcess = new ArrayList<>();
+		List<Integer> nums = new ArrayList<>();
 		
-		for(int c = 0; c < 6; c++) {
-			for(int i = 0; i < bags.size(); i++) {
-				String[] split = bags.get(i).split(" bags contain ");
-				
-				String goldHolder = split[0];
-				for(int j = 0; j < holder.size(); j++) {
-					
-					if(holder.get(j).contains(goldHolder)) {
-						String[] contains = split[1].split(", ");
-						for(int k = 0; k < contains.length; k++) {
-							if(split[1].equals("no other bags.")) {
-								break;
-							}
-							else {
-								quantities.put(contains[k].substring(2, contains[k].length() - 4), Integer.valueOf(contains[k].substring(0,1)));
-								holder.add(contains[k].substring(0, contains[k].length() - 4));
-							}
-							
-						}
-					}
+		bagsToProcess.add("shiny gold");
+		nums.add(1);
+		
+		while(!bagsToProcess.isEmpty()) {
+			String bag = bagsToProcess.get(bagsToProcess.size()-1);
+			int num = nums.get(nums.size() - 1);
+			bagsToProcess.remove(bagsToProcess.size() - 1);
+			nums.remove(nums.size() - 1);
+			
+			int j = 0;
+			while(!bags.get(j).startsWith(bag))
+				j++;
+			
+			String line = bags.get(j);
+			String[] contents = line.substring(line.indexOf("contain") + 8).replace(".", "").split(", ");
+			
+			if(!contents[0].equals("no other bags")) {
+				for(String content : line.substring(line.indexOf("contain") + 8).replace(".", "").split(", ")) {
+					int subCount = Integer.parseInt(content.substring(0, content.indexOf(" ")).trim());
+					bagsToProcess.add(content.substring(content.indexOf(" ")).replace("bags", "").trim());
+					nums.add(subCount * num);
+					count += subCount * num;
 				}
 			}
 		}
-		
-		Set<String> set = new LinkedHashSet<>(holder);
-		holder.clear();
-		holder.addAll(set);
-		
-		for(int i = 0; i < bags.size(); i++) {
-			String[] split = bags.get(i).split(" bags contain ");
-			
-
-			String[] contents = split[1].split(", ");
-			for(int j = 0; j < holder.size(); j++) {
-				String goldHolder = split[0];
-				if(holder.get(j).contains(goldHolder)) {
-					System.out.println(holder.get(j) + " layer " + j);
-					
-				} //I GIVE UP
-			}
-		}
+		System.out.println(count);
 	}
 }
